b92726d926a3f34c959b16f7a9b4a32a75ba65e1
simonator420 <salajsimon@gmail.com>
2024-07-11 10:27:54 +0200

    Retrieving user details

--- Events.php
+++ Events.php
@@ -7,6 +7,34 @@ use yii\base\Event;
 use yii\helpers\Url;
 use Yii;
 
+use humhub\components\behaviors\GUID;
+use humhub\libs\UUIDValidator;
+use humhub\modules\admin\Module as AdminModule;
+use humhub\modules\admin\permissions\ManageGroups;
+use humhub\modules\admin\permissions\ManageSpaces;
+use humhub\modules\admin\permissions\ManageUsers;
+use humhub\modules\content\components\ContentContainerActiveRecord;
+use humhub\modules\content\components\ContentContainerSettingsManager;
+use humhub\modules\content\models\Content;
+use humhub\modules\friendship\models\Friendship;
+use humhub\modules\space\helpers\MembershipHelper;
+use humhub\modules\space\models\Space;
+use humhub\modules\user\authclient\Password as PasswordAuth;
+use humhub\modules\user\behaviors\Followable;
+use humhub\modules\user\behaviors\ProfileController;
+use humhub\modules\user\components\ActiveQueryUser;
+use humhub\modules\user\components\PermissionManager;
+use humhub\modules\user\events\UserEvent;
+use humhub\modules\user\helpers\AuthHelper;
+use humhub\modules\user\Module;
+use humhub\modules\user\services\PasswordRecoveryService;
+use yii\base\Exception;
+use yii\base\InvalidConfigException;
+use yii\db\ActiveQuery;
+use yii\db\Expression;
+use yii\web\IdentityInterface;
+
+
 /**
  * Events class.
  * This class contains event handlers for the Employee Training module.
@@ -21,6 +49,12 @@ class Events
         /** @var \humhub\widgets\TopMenu $menu */
         $menu = $event->sender;
 
+
+        // Log message to PHP log file
+        Yii::info('Admin check in Employee Training module onTopMenuInit.');
+
+
+
         if (Yii::$app->user->isAdmin()) {
             $menu->addItem([
                 'label' => 'User Info',
@@ -29,21 +63,33 @@ class Events
                 'sortOrder' => 100,
                 'isActive' => (Yii::$app->controller->module && Yii::$app->controller->module->id == 'employeeTraining'),
             ]);
-            Yii::info('message log');
         }
     }
 
-    public static function onUserLogin(Event $event)
+    public static function onAfterLogin(Event $event)
     {
-        $user = $event->identity;
+        $user = Yii::$app->user;
+        $username = $user->identity->username;
+        $title =$user->identity->profile->title;
+        
 
-        // Check if the user has the title 'Driver'
-        if ($user->profile->title === 'Driver') {
-            // Set flash message to show driver popup
-            Yii::$app->session->setFlash('showDriverPopup', true);
-            
-            // Log a message to confirm the function is being executed
-            Yii::debug('Driver user logged in: ' . $user->username, __METHOD__);
+        Yii::info('User : ' . $username . ' with id: ' . $user->getId() . ' and title: ' . $title);
+
+        $userSpaces = $user->identity->getSpaces()->all();
+
+        if (!empty($userSpaces)) {
+            foreach ($userSpaces as $space) {
+                Yii::info($username . ' is a member of space: ' . $space->name);
+            }
         }
+
+        // // Check if the user has the title 'Driver'
+        // if ($user->profile->title === 'Driver') {
+        //     // Set flash message to show driver popup
+        //     Yii::$app->session->setFlash('showDriverPopup', true);
+            
+        //     // Log a message to confirm the function is being executed
+        //     Yii::debug('Driver user logged in: ' . $user->username, __METHOD__);
+        // }
     }
 }
--- config.php
+++ config.php
@@ -1,26 +1,21 @@
 <?php
 
 use humhub\widgets\TopMenu;
-// use humhub\modules\user\components\User;
-use yii\base\Event;
-use yii\web\User;
 use humhub\modules\employeeTraining\Events;
+use humhub\modules\user\controllers\AuthController;
 
 return [
     'id' => 'employeeTraining',
     'namespace' => 'humhub\modules\employeeTraining',
     'class' => 'humhub\modules\employeeTraining\Module',
     'events' => [
-        [
-            'class' => TopMenu::class, 
-            'event' => TopMenu::EVENT_INIT, 
-            'callback' => ['humhub\modules\employeeTraining\Events', 'onTopMenuInit']
-        ],
-        [
-            'class'=> User::className(),
-            // TODO EVENT AFTER LOGIN DOESNT EXIST
-            'event' => User::EVENT_AFTER_LOGIN,
-            'callback' => ['humhub\modules\employeeTraining\Events', 'onUserLogin']
-        ]
+        [TopMenu::class, TopMenu::EVENT_INIT, [Events::class, 'onTopMenuInit']],
+        [AuthController::class, AuthController::EVENT_AFTER_LOGIN, [Events::class, 'onAfterLogin']]
+        // [
+        //     'class'=> User::className(),
+        //     // TODO EVENT AFTER LOGIN DOESNT EXIST
+        //     'event' => User::EVENT_AFTER_LOGIN,
+        //     'callback' => ['humhub\modules\employeeTraining\Events', 'onUserLogin']
+        // ]
     ],
 ];
