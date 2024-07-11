ed7174c974bb88ba06387b9323222a233c9dcc26
simonator420 <salajsimon@gmail.com>
2024-07-11 15:47:04 +0200

    Added some comments

--- controllers/RoleController.php
+++ controllers/RoleController.php
@@ -6,35 +6,36 @@ use humhub\components\Controller;
 use humhub\modules\user\models\User;
 use Yii;
 
-/**
- * This controller handles actions related to roles within the Employee Training module.
- */
+
+// Controller for handling actions related to roles within the Employee Training module.
 class RoleController extends Controller
 {
-    /**
-     * This action fetches all users from the database and renders the 'user-info' view (displays user information).
-     */
+    
+    // Fetches all users from the database and renders the 'user-info' view (displays user information).    
     public function actionUserInfo()
     {
         // Fetch all users from the database
         $users = User::find()->all();
 
-        // Render the 'user-info' view and pass the users data to it
+        // Checks if the user isn't System Administrator
         if (Yii::$app->user->identity->profile->title != "System Administration") {
             return $this->redirect(['site/access-denied']);
         }
 
+        // Render the 'user-info' view and pass the users data to it
         return $this->render('user-info', [
             'users' => $users,
         ]);
     }
 
+    // Function for displaying training screen after login
+    // TODO rename the function so it suits all the employees
     public function actionDriver()
     {
         $user = Yii::$app->user;
         $title = $user->identity->profile->title;
 
-        // Check if the user is logged in and if their title is "Service Driver"
+        // Check if the user is logged in and if their title is not System Administrator
         if (!$user->isGuest && $title != 'System Administration') {
             Yii::info('User is Service Driver, rendering driver popup');
             return $this->render('driver', [
