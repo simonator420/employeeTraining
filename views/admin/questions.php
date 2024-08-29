1f3c9052c1c791c90f8465ec3cf62b8cc3183c8c
simonator420 <salajsimon@gmail.com>
2024-08-29 10:34:52 +0200

    User id

--- views/admin/questions.php
+++ views/admin/questions.php
@@ -358,7 +358,8 @@ $(document).on('click', '#assign-users-btn', function() {
                 var profilesHtml = '';
                 // Iterate over the profiles and create divs with checkboxes
                 $.each(response.profiles, function(index, profile) {
-                    profilesHtml += '<li><input type="checkbox" class="profile-checkbox" value="' + profile.id + '"' + 
+                    console.log('User ID:', profile.id, 'User Name:', profile.firstname + ' ' + profile.lastname);
+                    profilesHtml += '<div><input type="checkbox" class="profile-checkbox" value="' + profile.id + '"' + 
                                 (profile.isAssigned ? ' checked data-was-assigned="true"' : '') + '> ' + 
                                 profile.firstname + ' ' + profile.lastname + '</div>';
                 });
@@ -451,7 +452,8 @@ $(document).on('click', '#submit-filter-btn', function() {
             if (response.success) {
                 var usersHtml = '';
                 $.each(response.users, function(index, user) {
-                    usersHtml += '<li><input type="checkbox" class="profile-checkbox" value="' + user.id + '"> ' + user.firstname + ' ' + user.lastname + '</li>';
+                    console.log('User ID:', user.user_id, 'User Name:', user.firstname + ' ' + user.lastname);
+                    usersHtml += '<div><input type="checkbox" class="profile-checkbox" value="' + user.user_id + '"> ' + user.firstname + ' ' + user.lastname + '</div>';
                 });
                 $('#profile-list').html(usersHtml);
             } else {
