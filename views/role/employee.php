04dddbfd10e5d4255bdd864bd3d07f251b720440
simonator420 <salajsimon@gmail.com>
2024-08-29 11:45:29 +0200

    Implemented one by one question

--- views/role/employee.php
+++ views/role/employee.php
@@ -5,55 +5,65 @@ use yii\helpers\Url;
 
 ?>
 
-<div class="employee-training-container">
-    <div class="employee-training-card">
-
-        <h1><?= Yii::t('employeeTraining', 'Welcome to the ') ?> <b> <?= Html::encode($trainingId) ?> </b>
-            <?= Yii::t('employeeTraining', 'training') ?></h1>
-        <p class="welcome-text"><?= Yii::t('employeeTraining', 'Dear ') ?> <b>
-                <?= Html::encode($firstName) ?></b><?= Yii::t('employeeTraining', ', you have been assigned this training. Please complete it at your earliest convenience. Please note that if the page is refreshed during the training, your inputs will not be saved.') ?>
-        </p><br>
-
-        <!-- Video container with end button -->
-        <?php
-        $fileUrl = Yii::$app->db->createCommand('
+<?php
+$fileUrl = Yii::$app->db->createCommand('
             SELECT initial_file_url 
             FROM training 
             WHERE id = :trainingId
             ')
-            ->bindValue(':trainingId', $trainingId)
-            ->queryScalar();
-        ?>
-        <!-- If the initial file was set -->
-        <?php if ($fileUrl != null): ?>
-            <?php
+    ->bindValue(':trainingId', $trainingId)
+    ->queryScalar();
+if ($fileUrl != null):
+    // Determine the file type based on its extension
+    $fileExtension = pathinfo($fileUrl, PATHINFO_EXTENSION);
+endif;
+?>
 
-            // Determine the file type based on its extension
-            $fileExtension = pathinfo($fileUrl, PATHINFO_EXTENSION);
-            ?>
-            <div class="form-group" id="file-container" style="text-align: center;">
-                <!-- Display appropriate content based on the file type -->
-                <?php if (in_array($fileExtension, ['mp4', 'webm', 'ogg'])): ?>
-                    <video id="training-video" width="auto" controls>
-                        <source src="<?= Url::to('@web/' . $fileUrl) ?>" type="video/<?= $fileExtension ?>">
-                        Your browser does not support the video tag.
-                    </video><br>
-                <?php elseif ($fileExtension === 'pdf'): ?>
-                    <?php $pdfUrl = Url::to('@web/' . $fileUrl) . '?t=' . time(); ?>
-                    <embed src="<?= $pdfUrl ?>" width="90%" type="application/pdf" style="min-height: 65vh;" alt="pdf" /><br>
-                <?php endif; ?>
+<div class="employee-training-container">
+    <div class="employee-training-card"
+        style="height: 70vh; display: flex; flex-direction: column; justify-content: space-between;">
+        <div class="training-content" style="flex-grow: 1; overflow-y: auto;">
+            <h1><?= Yii::t('employeeTraining', 'Welcome to the ') ?> <b> <?= Html::encode($trainingId) ?> </b>
+                <?= Yii::t('employeeTraining', 'training') ?></h1>
+            <p class="welcome-text"><?= Yii::t('employeeTraining', 'Dear ') ?> <b>
+                    <?= Html::encode($firstName) ?></b><?= Yii::t('employeeTraining', ', you have been assigned this training. Please complete it at your earliest convenience. Please note that if the page is refreshed during the training, your inputs will not be saved.') ?>
+            </p><br>
 
-                <button id="end-file-btn" class="btn btn-secondary"><?= Yii::t('employeeTraining', 'Continue') ?></button>
+            <?php if ($fileUrl != null): ?>
+                <div class="form-group" id="file-container" style="text-align: center;">
+                    <?php if (in_array($fileExtension, ['mp4', 'webm', 'ogg'])): ?>
+                        <video id="training-video" width="auto" controls>
+                            <source src="<?= Url::to('@web/' . $fileUrl) ?>" type="video/<?= $fileExtension ?>">
+                            Your browser does not support the video tag.
+                        </video><br>
+                    <?php elseif ($fileExtension === 'pdf'): ?>
+                        <?php $pdfUrl = Url::to('@web/' . $fileUrl) . '?t=' . time(); ?>
+                        <embed src="<?= $pdfUrl ?>" width="90%" type="application/pdf" style="min-height: 65vh;"
+                            alt="pdf" /><br>
+                    <?php endif; ?>
+
+                    <button id="end-file-btn"
+                        class="btn btn-secondary"><?= Yii::t('employeeTraining', 'Continue') ?></button>
+                </div>
+
+                <div id="questions-container" style="display: none;"></div>
+                <?= Html::button(Yii::t('employeeTraining', 'Next'), ['class' => 'btn btn-primary', 'id' => 'next-btn', 'style' => 'display: none; ']) ?>
+                <?= Html::button(Yii::t('employeeTraining', 'Previous'), ['class' => 'btn btn-secondary', 'id' => 'prev-btn', 'style' => 'display: none;']) ?>
+                <?= Html::a(Yii::t('employeeTraining', 'Submit'), Url::to(['/dashboard']), ['class' => 'btn btn-success', 'id' => 'submit-btn', 'style' => 'display: none;']) ?>
+            <?php else: ?>
+                <div id="questions-container"></div>
+                <?= Html::button(Yii::t('employeeTraining', 'Next'), ['class' => 'btn btn-primary', 'id' => 'next-btn']) ?>
+                <?= Html::button(Yii::t('employeeTraining', 'Previous'), ['class' => 'btn btn-secondary', 'id' => 'prev-btn', 'style' => 'display: none;']) ?>
+                <?= Html::a(Yii::t('employeeTraining', 'Submit'), Url::to(['/dashboard']), ['class' => 'btn btn-success', 'id' => 'submit-btn', 'style' => 'display: none;']) ?>
+            <?php endif; ?>
+            <div id="question-navigation" style="text-align: center; margin-top: 20px;">
+                <!-- Question numbers will be dynamically added here -->
             </div>
-
-            <div id="questions-container" style="display: none;"></div>
-            <?= Html::a(Yii::t('employeeTraining', 'Submit'), Url::to(['/dashboard']), ['class' => 'btn btn-primary', 'id' => 'submit-btn', 'style' => 'display: none;']) ?>
-        <?php else: ?>
-            <div id="questions-container"></div>
-            <?= Html::a(Yii::t('employeeTraining', 'Submit'), Url::to(['/dashboard']), ['class' => 'btn btn-primary', 'id' => 'submit-btn']) ?>
-        <?php endif; ?>
+        </div>
     </div>
 </div>
+
+
 
 <?php
 // URL to display questions based on the title
@@ -65,6 +75,7 @@ var trainingId = '{$trainingId}';
 // Document ready function to initialize when the page is loaded
 $(document).ready(function() {
     var trainingId = '{$trainingId}';
+    var currentQuestionIndex = 0;
 
     function loadQuestions() {
         $.ajax({
@@ -76,6 +87,8 @@ $(document).ready(function() {
                 if (response.success) {
                     // Display the questions in the container
                     $('#questions-container').html(response.html);
+                    generateQuestionNavigation();
+                    showQuestion(currentQuestionIndex);
                 } else {
                     // Display a message if no questions are available
                     $('#questions-container').html('<p>No questions available.</p>');
@@ -87,10 +100,63 @@ $(document).ready(function() {
         });
     }
 
-    // Load questions when the page is ready
-    loadQuestions();
+    function generateQuestionNavigation() {
+        var totalQuestions = $('.question-item').length;
+        var navHtml = '';
 
-    checkMultipleChoiceSelection();
+        for (var i = 0; i < totalQuestions; i++) {
+            navHtml += '<button class="question-nav-btn" data-index="' + i + '">' + (i + 1) + '</button>';
+        }
+
+        $('#question-navigation').html(navHtml);
+        highlightCurrentQuestion(currentQuestionIndex);
+    }
+
+    function highlightCurrentQuestion(index) {
+        $('.question-nav-btn').removeClass('active');
+        $('.question-nav-btn[data-index="' + index + '"]').addClass('active');
+    }
+
+    function showQuestion(index) {
+        var totalQuestions = $('.question-item').length;
+        $('.question-item').hide();
+        $('.question-item').eq(index).show();
+
+        if (index === 0) {
+            $('#prev-btn').hide();
+        } else {
+            $('#prev-btn').show();
+        }
+
+        if (index === totalQuestions - 1) {
+            $('#next-btn').hide();
+            $('#submit-btn').show();
+        } else {
+            $('#next-btn').show();
+            $('#submit-btn').hide();
+        }
+
+        highlightCurrentQuestion(index);
+    }
+
+    $('#next-btn').on('click', function() {
+        if (currentQuestionIndex < $('.question-item').length - 1) {
+            currentQuestionIndex++;
+            showQuestion(currentQuestionIndex);
+        }
+    });
+
+    $('#prev-btn').on('click', function() {
+        if (currentQuestionIndex > 0) {
+            currentQuestionIndex--;
+            showQuestion(currentQuestionIndex);
+        }
+    });
+
+    $(document).on('click', '.question-nav-btn', function() {
+        currentQuestionIndex = $(this).data('index');
+        showQuestion(currentQuestionIndex);
+    });
 
     // Function to hide the video and show the questions
     $('#end-file-btn').on('click', function(e) {
@@ -99,89 +165,84 @@ $(document).ready(function() {
             videoElement.pause();
             videoElement.currentTime = 0;
         }
-        document.getElementById('file-container').style.display = 'none';
-        document.getElementById('questions-container').style.display = 'block';
-        document.getElementById('submit-btn').style.display = 'inline';
+        $('#file-container').hide();
+        $('#questions-container').show();
+        loadQuestions();  // Display the first question
+        $('#next-btn').show();  // Show the "Next" button after questions are loaded
     });
 
-    // Flag to check if training is completed
-    let trainingCompleted = false;
-
-    // Event handler for the submit button click
     $('#submit-btn').on('click', function(e) {
         e.preventDefault();
-        if (!trainingCompleted) {
-            let isValid = true;
-            let data = { _csrf: yii.getCsrfToken(), training_id: trainingId, TrainingQuestions: {} };
+        let isValid = true;
+        let data = { _csrf: yii.getCsrfToken(), training_id: trainingId, TrainingQuestions: {} };
 
-            // Collect answers for text, number, and range inputs
-            $('.question-input').each(function() {
-                let questionId = $(this).data('question-id');
-                let questionText = $(this).data('question-text');
-                let questionType = $(this).data('question-type');
-                let inputValue = $(this).val();
+        $('.question-input').each(function() {
+            let questionId = $(this).data('question-id');
+            let questionText = $(this).data('question-text');
+            let questionType = $(this).data('question-type');
+            let inputValue = $(this).val();
 
-                if (!inputValue) {
-                    isValid = false;
-                    $(this).css('border', '2px solid red');
-                } else {
-                    $(this).css('border', '1px solid #dee2e6');
-                }
+            if (!inputValue) {
+                isValid = false;
+                $(this).css('border', '2px solid red');
+            } else {
+                $(this).css('border', '1px solid #dee2e6');
+            }
 
+            data.TrainingQuestions[questionId] = {
+                question_id: questionId,
+                question: questionText,
+                answer: inputValue,
+                question_type: questionType
+            };
+        });
+
+        $('.multiple-choice-option').each(function() {
+            let questionId = $(this).data('question-id');
+            let questionText = $(this).data('question-text');
+            let questionType = $(this).data('question-type');
+
+            if (!data.TrainingQuestions[questionId]) {
                 data.TrainingQuestions[questionId] = {
                     question_id: questionId,
                     question: questionText,
-                    answer: inputValue,
+                    answer: [],
                     question_type: questionType
                 };
-            });
-
-            // Collect multiple-choice answers
-            $('.multiple-choice-option').each(function() {
-                let questionId = $(this).data('question-id');
-                let questionText = $(this).data('question-text');
-                let questionType = $(this).data('question-type');
-
-                // Initialize the question if it hasn't been added yet
-                if (!data.TrainingQuestions[questionId]) {
-                    data.TrainingQuestions[questionId] = {
-                        question_id: questionId,
-                        question: questionText,
-                        answer: [],
-                        question_type: questionType
-                    };
-                }
-
-                // Add the value if the option is checked
-                if ($(this).is(':checked')) {
-                    data.TrainingQuestions[questionId].answer.push($(this).val());
-                }
-            });
-
-            if (isValid) {
-                $.ajax({
-                    url: '$completeTrainingUrl',
-                    type: 'POST',
-                    data: data,
-                    success: function(response) {
-                        if (response.success) {
-                            alert('Thank you for completing the training!');
-                            trainingCompleted = true;
-                            var currentTime = new Date().toISOString().slice(0, 19).replace('T', ' ');
-                            $('#training-complete-time-' + response.userId).text(currentTime);
-                            window.location.href = $('#submit-btn').attr('href');
-                        } else {
-                            alert('Failed to complete the training. Please try again or contact System Administrator.');
-                        }
-                    },
-                    error: function(xhr, status, error) {
-                        console.error('AJAX Error:', status, error);
-                        alert('Error in AJAX request. Please try again or contact System Administrator.');
-                    }
-                });
             }
+
+            if ($(this).is(':checked')) {
+                data.TrainingQuestions[questionId].answer.push($(this).val());
+            }
+        });
+
+        if (isValid) {
+            $.ajax({
+                url: '$completeTrainingUrl',
+                type: 'POST',
+                data: data,
+                success: function(response) {
+                    if (response.success) {
+                        alert('Thank you for completing the training!');
+                        window.location.href = $('#submit-btn').attr('href');
+                    } else {
+                        alert('Failed to complete the training. Please try again.');
+                    }
+                },
+                error: function(xhr, status, error) {
+                    alert('Error in AJAX request. Please try again.');
+                }
+            });
         }
     });
+
+    // Initially hide the "Next" button if a video or PDF is present
+    if ($('#file-container').length > 0) {
+        $('#next-btn').hide();
+    }
+
+    $('#prev-btn').hide();
+    $('#submit-btn').hide();
 
     function checkMultipleChoiceSelection() {
         if ($('.multiple-choice-option:checked').length === 0) {
